8bce4b1ca8daa52e082ae64c50ff830d0edbd295
Calen Pennington <calen.pennington@gmail.com>
2012-08-09 11:40:27 -0400

    Add rake file for builds and tests

--- xqueue/test_settings.py
+++ xqueue/test_settings.py
@@ -5,3 +5,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
     }
 }
+
+# Nose Test Runner
+INSTALLED_APPS += ('django_nose',)
+NOSE_ARGS = ['--cover-erase', '--with-xunit', '--with-xcoverage', '--cover-html',
+             '--cover-inclusive', '--cover-html-dir',
+             os.environ.get('NOSE_COVER_HTML_DIR', 'cover_html'),
+             '--cover-package', 'queue']
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
