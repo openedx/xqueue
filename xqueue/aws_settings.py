c2238b719d0e308090c549b42cd530ff86bb4959
Calen Pennington <calen.pennington@gmail.com>
2012-08-13 12:02:05 -0400

    Add logging settings

--- xqueue/aws_settings.py
+++ xqueue/aws_settings.py
@@ -1,12 +1,17 @@
 from settings import *
-
-ENV_ROOT = REPO_PATH.dirname()
+from logsettings import get_logger_config
 
 with open(ENV_ROOT / "env.json") as env_file:
     ENV_TOKENS = json.load(env_file)
 
 XQUEUES = ENV_TOKENS['XQUEUES']
 XQUEUE_WORKERS_PER_QUEUE = ENV_TOKENS['XQUEUE_WORKERS_PER_QUEUE']
+
+LOG_DIR = ENV_TOKENS['LOG_DIR']
+LOGGING = get_logger_config(LOG_DIR,
+                            logging_env=ENV_TOKENS['LOGGING_ENV'],
+                            syslog_addr=(ENV_TOKENS['SYSLOG_SERVER'], 514),
+                            debug=False)
 
 with open(ENV_ROOT / "auth.json") as auth_file:
     AUTH_TOKENS = json.load(auth_file)
--- xqueue/settings.py
+++ xqueue/settings.py
@@ -1,9 +1,10 @@
 import os
-import json
+from logsettings import get_logger_config
 from path import path
 
 ROOT_PATH = path(__file__).dirname()
 REPO_PATH = ROOT_PATH.dirname()
+ENV_ROOT = REPO_PATH.dirname()
 
 # Django settings for xqueue project.
 
@@ -138,26 +139,6 @@ LOGIN_URL = '/xqueue/login'
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+LOGGING = get_logger_config(ENV_ROOT / "log",
+                            logging_env="dev",
+                            debug=True)
