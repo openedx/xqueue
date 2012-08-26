149be1f1b0ac46cf36dd73ead8dd6b4692eefef5
kimth <kimth@edx.org>
2012-08-26 19:00:32 +0000

    Requeue command by default processes all queues

--- queue/management/commands/requeue_pulled_submissions.py
+++ queue/management/commands/requeue_pulled_submissions.py
@@ -13,14 +13,16 @@ log = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     args = "<queue_name>"
-    help = "Process currently open, pulled submissions on queue <queue_name>, and requeue if external grader has not replied in settings.PULLED_SUBMISSION_TIMEOUT seconds" 
+    help = "Process currently pulled submissions, and requeue if external grader has not replied in settings.PULLED_SUBMISSION_TIMEOUT seconds. Optional <queue_name>" 
 
     def handle(self, *args, **options):
         log.info(' [*] Running requeue of pulled submissions...')
 
-        queue_name = args[0]
-
-        open_submissions = Submission.objects.filter(queue_name=queue_name, lms_ack=False) 
+        if len(args) > 0:
+            queue_name = args[0]
+            open_submissions = Submission.objects.filter(queue_name=queue_name, lms_ack=False) 
+        else:
+            open_submissions = Submission.objects.filter(lms_ack=False)
         open_submissions = open_submissions.exclude(pull_time=None)
 
         for open_submission in open_submissions:
@@ -29,12 +31,13 @@ class Command(BaseCommand):
             if time_difference > settings.PULLED_SUBMISSION_TIMEOUT:
                 open_submission.num_failures += 1
                 if open_submission.num_failures < settings.MAX_NUMBER_OF_FAILURES:
-                    log.info(' [ ] Requeuing submission.id=%d which has been outstanding for %d seconds' % (open_submission.id, time_difference))
+                    log.info(' [ ] Requeuing submission.id=%d to queue %s which has been outstanding for %d seconds' %\
+                        (open_submission.id, open_submission.queue_name, time_difference))
                     qitem = str(open_submission.id)
                     open_submission.pull_time = None
-                    push_to_queue(queue_name, qitem)
+                    push_to_queue(open_submission.queue_name, qitem)
                 else:
-                    log.info(' [ ] NOT requeueing submission.id=%d because num_failures=%d >= MAX_NUMBER_OF_FAILURES=%d' %\
-                                (open_submission.id, open_submission.num_failures, settings.MAX_NUMBER_OF_FAILURES))
+                    log.info(' [ ] NOT requeueing submission.id=%d to queue %s because num_failures=%d >= MAX_NUMBER_OF_FAILURES=%d' %\
+                                (open_submission.id, open_submission.queue_name, open_submission.num_failures, settings.MAX_NUMBER_OF_FAILURES))
                 open_submission.save()
                 #post_failure_to_lms(open_submission.xqueue_header) # TODO: Requeue, not retire 
