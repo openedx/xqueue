555333aa460157edd8f966bd0b6a208fa9f9b2bb
kimth <kimth@edx.org>
2012-08-26 19:25:54 +0000

    Force switch for retiring submissions

--- queue/management/commands/retire_submissions.py
+++ queue/management/commands/retire_submissions.py
@@ -2,6 +2,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from optparse import make_option
 from queue.models import Submission
 from queue.consumer import post_failure_to_lms
 
@@ -11,6 +12,14 @@ log = logging.getLogger(__name__)
 class Command(BaseCommand):
     args = "<queue_name>"
     help = "Retire submissions that have more than settings.MAX_NUMBER_OF_FAILURES failures. Notify the LMS/student that the queue will no longer attempt to process the submission. Optional <queue_name>"
+
+    option_list = BaseCommand.option_list + (
+        make_option('-f','--force',
+            action='store_true',
+            dest='force',
+            default=False,
+            help='Force retire submissions'),
+        )
 
     def handle(self, *args, **options):
         log.info(' [*] Scanning Submission database to retire failed submissions')
@@ -29,6 +38,7 @@ class Command(BaseCommand):
                             (failed_submission.id, failed_submission.queue_name, failed_submission.num_failures))
                 failed_submission.lms_ack = post_failure_to_lms(failed_submission.xqueue_header)
                 if not failed_submission.lms_ack:
-                    log.info(' [ ] Could not contact LMS to retire submission id=%d' % failed_submission.id)
-                    failed_submission.lms_ack = True # Force retire. TODO: make a command switch -f
+                    log.error(' [ ] Could not contact LMS to retire submission id=%d' % failed_submission.id)
+                    if options['force']:
+                        failed_submission.lms_ack = True # Forcibly mark as done
                 failed_submission.save()
