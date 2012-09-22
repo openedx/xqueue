056628c46d5000da667e79600c9e37ba78f1136d
kimth <kimth@edx.org>
2012-09-22 20:40:27 +0000

    Remove some trailing whitespace

--- queue/ext_interface.py
+++ queue/ext_interface.py
@@ -1,17 +1,17 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 
 import json
 import logging
 
-from queue.models import Submission 
+from queue.models import Submission
 from queue.views import compose_reply
 from util import *
 
-import queue.producer 
+import queue.producer
 import queue.consumer
 
 log = logging.getLogger(__name__)
