febbbbc86459f4966ad906fb0613a385302763d4
kimth <kimth@edx.org>
2012-09-22 21:28:46 +0000

    Requeue and retire logic uses the new retired column of Submission

--- queue/management/commands/retire_submissions.py
+++ queue/management/commands/retire_submissions.py
@@ -25,14 +25,16 @@ class Command(BaseCommand):
         log.info(' [*] Scanning Submission database to retire failed submissions...')
         
         force = options['force']
+        if force:
+            log.info(" [ ] Force retiring all failed submissions...")
 
         if len(args) == 0:
-            failed_submissions = Submission.objects.filter(lms_ack=False)
+            failed_submissions = Submission.objects.filter(retired=False)
             failed_submissions = failed_submissions.exclude(num_failures=0)
             self.retire_submissions(failed_submissions, force)
         else:
             for queue_name in args:
-                failed_submissions = Submission.objects.filter(queue_name=queue_name, lms_ack=False)
+                failed_submissions = Submission.objects.filter(queue_name=queue_name, retired=False)
                 failed_submissions = failed_submissions.exclude(num_failures=0)
                 self.retire_submissions(failed_submissions, force)
         
@@ -43,10 +45,10 @@ class Command(BaseCommand):
                 log.info(" [ ] Retiring submission id=%d from queue '%s' with num_failures=%d" %\
                             (failed_submission.id, failed_submission.queue_name, failed_submission.num_failures))
                 if force:
-                    log.info(" [ ] Force retiring all failed submissions...")
-                    failed_submission.lms_ack = True # Mark as done without contacting LMS
+                    failed_submission.retired = True # Mark as done without contacting LMS
                 else:
                     failed_submission.lms_ack = post_failure_to_lms(failed_submission.xqueue_header)
+                    failed_submission.retired = failed_submission.lms_ack
                     if not failed_submission.lms_ack:
                         log.error(' [ ] Could not contact LMS to retire submission id=%d' % failed_submission.id)
                 failed_submission.save()
