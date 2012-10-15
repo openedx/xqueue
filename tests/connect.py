3eb516fdbb3c3e2ecbe140dc2113f604233b29d4
Ned Batchelder <ned@nedbatchelder.com>
2012-10-15 12:10:57 -0400

    Refactor more of the xqueue test so we can invoke it from a load test also.

--- tests/connect.py
+++ tests/connect.py
@@ -7,6 +7,7 @@ import argparse
 import datetime
 import itertools
 import json
+import logging
 import requests
 import pprint
 import sys
@@ -18,18 +19,25 @@ now = datetime.datetime.now
 QSERVER = 'http://127.0.0.1:3032'
 PWD = 'abcd'
 
-RUN = now().strftime("%Y%m%d%H%M%S")
+logger = logging.getLogger(__name__)
 
-def send_test_requests(qserver, pwd, threadid, rate=None):
 
-    print "logging in..."
-    s = requests.session()
-    r = s.post(qserver + '/xqueue/login/',  {'username': 'lms', 'password': pwd})
-    r.raise_for_status()
-    print r.json
+class XQueueClient(object):
+    RUN = now().strftime("%Y%m%d%H%M%S")
 
-    for student in itertools.count():
-        good_request = {
+    def __init__(self, server, pwd):
+        self.s = requests.session()
+        self.server = server
+        self.pwd = pwd
+
+    def login(self):
+        logger.info("logging in...")
+        r = self.s.post(self.server + '/xqueue/login/',  {'username': 'lms', 'password': self.pwd})
+        r.raise_for_status()
+        logger.debug("login response: %r", r.json)
+
+    def make_request(self, uniqueid):
+        request = {
             'xqueue_header': json.dumps({
                 'lms_callback_url': 'http://httpbin.org/post',  # an actual mock http server online.
                 'lms_key': 'qwerty',
@@ -37,7 +45,7 @@ def send_test_requests(qserver, pwd, threadid, rate=None):
             }),
             'xqueue_body': json.dumps({
                 'student_info': json.dumps({
-                    'anonymous_student_id': "connect_py_%s_%03d_%05d" % (RUN, threadid, student), 
+                    'anonymous_student_id': "connect_py_%s_%s" % (self.RUN, uniqueid), 
                     'submission_time': now().strftime("%Y%m%d%H%M%S"),
                 }),
                 'grader_payload': json.dumps({
@@ -56,24 +64,35 @@ def send_test_requests(qserver, pwd, threadid, rate=None):
                     "    print('smaller')\n",
             }), 
         }
+        return request
 
-        print "sending job..."
-        r = s.post(qserver + '/xqueue/submit/',  good_request)
+    def submit_job(self, request):
+        logger.info("submitting job...")
+        r = self.s.post(self.server + '/xqueue/submit/', request)
         r.raise_for_status()
-        print r.json
+        logger.debug("submit response: %r", r.json)
+
+
+def send_test_requests(qserver, pwd, threadid, rate=None):
+    """Sent a number of XQueue requests."""
+    xq = XQueueClient(qserver, pwd)
+    xq.login()
+
+    for student in itertools.count():
+        request = xq.make_request("%03d_%05d" % (threadid, student))
+        xq.submit_job(request)
         if not rate:
             break
         time.sleep(1.0/rate)
 
-def main(args):
-    global qserver
-    global pwd
 
+def main(args):
     parser = argparse.ArgumentParser(description="Send dummy requests to a qserver")
     parser.add_argument('server', nargs='?')
     parser.add_argument('password', nargs='?')
     parser.add_argument('--rate', help="How many requests per second per thread to send.", type=int)
     parser.add_argument('--threads', help="How many threads to run.", type=int)
+    parser.add_argument('--quiet', help="Be chatty?", action='store_true')
 
     args = parser.parse_args()
 
@@ -83,6 +102,14 @@ def main(args):
     if qserver.endswith('/'):
         # strip off trailing /
         qserver = qserver[:-1]
+
+    # Other components (like requests) spew log stuff we don't want to see.
+    logging.basicConfig(
+        level=logging.WARNING,
+        format='%(message)s',
+    )
+
+    logger.setLevel(logging.WARNING if args.quiet else logging.DEBUG)
 
     if args.threads:
         for threadid in range(args.threads):
