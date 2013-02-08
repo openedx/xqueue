fc93e19c1fd0eb40b259500c379d076d2d507c1f
Calen Pennington <calen.pennington@gmail.com>
2013-02-08 15:48:08 -0500

    Fix usage of super

--- queue/consumer.py
+++ queue/consumer.py
@@ -174,7 +174,7 @@ class SingleChannel(multiprocessing.Process):
     Encapsulation of a single RabbitMQ listener that listens on multiple queues
     '''
     def __init__(self, worker_id, queues):
-        super(SingleChannel, self).__init__(self)
+        super(SingleChannel, self).__init__()
         self.worker_id = worker_id
         self.queues = queues
 
