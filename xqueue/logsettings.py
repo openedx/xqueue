c6a3f57560d3004df4bab9babafad336c4d658e6
Cory Lee <cory@edx.org>
2018-01-05 13:03:54 -0500

    Trying something

--- xqueue/logsettings.py
+++ xqueue/logsettings.py
@@ -63,7 +63,7 @@ def get_logger_config(log_dir,
             'syslogger-remote': {
                 'level': 'INFO',
                 'class': 'logging.handlers.SysLogHandler',
-                'address': syslog_addr,
+                'address': syslog_addr or 'localhost',
                 'formatter': 'syslog_format',
             },
         },
