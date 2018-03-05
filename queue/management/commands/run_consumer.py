40c71af3724cc945e21ee3ac4aec1ad24f2a6be8
Kevin Falcone <kevin@edx.org>
2018-03-05 15:31:42 -0500

    run_consumer had 2 unused functions and an unused class
    The tests only tested the unused functions.
    The rest of this needs to be tested in the test_consumer class

--- queue/management/commands/run_consumer.py
+++ queue/management/commands/run_consumer.py
@@ -14,54 +14,6 @@ log = logging.getLogger(__name__)
 MONITOR_SLEEPTIME = 10
 
 
-class NoAssignmentError(Exception):
-    pass
-
-
-def assign_workers_to_queues(queues, number_of_workers, workers_per_queue, minimum_disjoint_workers):
-    """
-    Returns a map of queues to worker ids (integers in range(number_of_workers))
-
-    The function guarantees that each queue is assigned to workers_per_queue workers,
-    and that each pair of queues has at least minimum_disjoint_workers workers
-    that are distinct. This ensures that if one queue is overloaded, the other queues will
-    still have active workers.
-
-    If no such assignment is possible, then will raise NoAssignmentError
-    """
-    workers = range(number_of_workers)
-    num_queues = len(queues)
-
-    possible_assignments = (set(combo) for combo in combinations(workers, workers_per_queue))
-    queue_assignments = []
-    for next_assignment in possible_assignments:
-        if len(queue_assignments) >= num_queues:
-            break
-
-        if min([len(next_assignment - old) for old in queue_assignments] + [workers_per_queue]) >= minimum_disjoint_workers:
-            queue_assignments.append(next_assignment)
-
-    if len(queue_assignments) < num_queues:
-        raise NoAssignmentError()
-
-    return dict(zip(queues, queue_assignments))
-
-
-def assign_queues_to_workers(num_workers, queue_assignments):
-    """
-    Takes a dictionary mapping queues to workers for those queues, and converts
-    it to a set of queues assigned to each worker
-
-    queue_assignments: Dict from queue -> worker ids (ints)
-    """
-    worker_assignments = [set() for worker in range(num_workers)]
-    for queue, assignment in queue_assignments.items():
-        for worker in assignment:
-            worker_assignments[worker].add(queue)
-
-    return worker_assignments
-
-
 class Command(BaseCommand):
     help = """
     Listens to all queues specified as being push-queues in the django
