5d9af61425b0b17c5b70d9c4bea632623f6817e9
Kevin Falcone <kevin@edx.org>
2018-03-09 13:42:28 -0500

    auto_now changes arrival on every update
    This isn't what this field is documented to do, so switch it to only set
    on creation.

---
+++ queue/migrations/0002_stop_updating_arrival_time.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('queue', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='submission',
+            name='arrival_time',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+    ]
