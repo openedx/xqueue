1526cd7b19f6a81278097756007611f6b92c85c4
Kevin Falcone <kevin@edx.org>
2018-05-03 13:38:38 -0400

    It was always ridiculous that this read your json file directly
    USERS is pushed into the Django config, so just walk that list instead.
    This broke when test_settings fully moved away from the json parsing

--- queue/management/commands/update_users.py
+++ queue/management/commands/update_users.py
@@ -1,14 +1,12 @@
 """
 Ensure that the right users exist:
 
-- read USERS dictionary from auth.json
+- read USERS dictionary from settings
 - if they don't exist, create them.
 - if they do, update the passwords to match
 
 """
-import json
 import logging
-import os
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -18,31 +16,21 @@ log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Create users that are specified in auth.json"
+    help = "Create users that are specified in your configuration"
 
     def handle(self, *args, **options):
 
-        log.info("root is : " + settings.ENV_ROOT)
-        auth_filename = getattr(settings, 'AUTH_FILENAME', 'xqueue.auth.json')
-        auth_path = os.path.join(settings.ENV_ROOT, auth_filename)
+        for username, pwd in settings.USERS.items():
+            log.info(' [*] Creating/updating user {0}'.format(username))
+            try:
+                user = User.objects.get(username=username)
+                user.set_password(pwd)
+                user.save()
+            except User.DoesNotExist:
+                log.info('     ... {0} does not exist. Creating'.format(username))
 
-        log.info(' [*] reading {0}'.format(auth_path))
-
-        with open(auth_path) as auth_file:
-            AUTH_TOKENS = json.load(auth_file)
-            users = AUTH_TOKENS.get('USERS', {})
-            for username, pwd in users.items():
-                log.info(' [*] Creating/updating user {0}'.format(username))
-                try:
-                    user = User.objects.get(username=username)
-                    user.set_password(pwd)
-                    user.save()
-                except User.DoesNotExist:
-                    log.info('     ... {0} does not exist. Creating'.format(username))
-
-                    user = User.objects.create(username=username,
-                                               email=username + '@dummy.edx.org',
-                                               is_active=True)
-                    user.set_password(pwd)
-                    user.save()
-        log.info(' [*] All done!')
+                user = User.objects.create(username=username,
+                                           email=username + '@dummy.edx.org',
+                                           is_active=True)
+                user.set_password(pwd)
+                user.save()
