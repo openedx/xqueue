f0b960d9b6172745759acb823002814bcdacd999
Kevin Falcone <kevin@edx.org>
2018-05-16 15:27:07 -0400

    Add a retire_old_submission command
    It requires a queue and also accepts a "retire older" command if you
    only want to retire things older than a certain amount.

--- queue/management/commands/tests/__init__.py
+++ queue/management/commands/tests/__init__.py
@@ -10,6 +10,7 @@ def bulk_create_submissions(count=1, days_old=10, **create_params):
         actual_arrival_time = datetime.now(pytz.utc) - timedelta(days=days_old)
         Submission.objects.filter(pk=submission.id).update(arrival_time=actual_arrival_time)
 
+
 def create_submission(**create_params):
     submission_params = dict(
         retired=0,
@@ -17,4 +18,3 @@ def create_submission(**create_params):
     )
     submission_params.update(create_params)
     return Submission.objects.create(**submission_params)
-
---
+++ queue/management/commands/tests/test_retire_old_submissions.py
@@ -0,0 +1,60 @@
+"""
+Tests of the update_users management command.
+"""
+from __future__ import absolute_import
+
+from datetime import timedelta
+from queue.management.commands.tests import bulk_create_submissions
+from queue.models import Submission
+
+import pytest
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.test import TestCase
+from django.utils import timezone
+from mock import patch
+
+
+class RetireOldSubmissionsTest(TestCase):
+
+    def count_unretired(self, count):
+        self.assertEquals(Submission.objects.filter(retired=False).count(),
+                          count)
+
+    @patch('queue.management.commands.retire_old_submissions.post_failure_to_lms', return_value=True)
+    def test_deletes(self, mock_post_to_lms):
+        "Retire all the submissions"
+        bulk_create_submissions(5)
+        self.count_unretired(5)
+        call_command('retire_old_submissions', 'test')
+        self.count_unretired(0)
+
+    @patch('queue.management.commands.retire_old_submissions.post_failure_to_lms', return_value=True)
+    def test_undeleted(self, mock_post_to_lms):
+        """
+        Create some older submissions and only retire those old ones
+        Defaults are 10 days old, add 4 day old ones and retire anything 5 days or older
+        """
+        bulk_create_submissions(5, 4)
+        bulk_create_submissions(5)
+        self.count_unretired(10)
+        older = timezone.now() - timedelta(5)
+        call_command('retire_old_submissions', 'test', retire_before=older.isoformat())
+        self.count_unretired(5)
+
+    @patch('queue.management.commands.retire_old_submissions.log')
+    @patch('queue.management.commands.retire_old_submissions.post_failure_to_lms', return_value=False)
+    def test_deletes_failing_to_connect_to_lms(self, mock_post_to_lms, mock_logger):
+        "Retire all the submissions, and fail the LMS callback"
+
+        bulk_create_submissions(5)
+        self.count_unretired(5)
+        call_command('retire_old_submissions', 'test')
+        mock_logger.error.assert_called()
+        logs, _ = mock_logger.error.call_args
+        self.assertRegexpMatches(logs[0], r'Could not contact LMS to retire submission')
+        self.count_unretired(0)
+
+    def test_bad_arguments(self):
+        with self.assertRaisesRegexp(CommandError, 'unable to parse datetime'):
+            call_command('retire_old_submissions', 'test', retire_before='2018-04-03')
