74b2b61c04817af1f4706b88be0a3267660a6b05
Zainab Amir <40633976+zainab-amir@users.noreply.github.com>
2019-10-31 22:28:31 +0500

    Rename djangoapp app (#575)
    "queue" app name conflicts with a module of urllib3 which is installed as
    a dependency for requests package.
    
    PROD-389

--- submission_queue/migrations/0003_compound_indexes.py
+++ submission_queue/migrations/0003_compound_indexes.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('queue', '0002_stop_updating_arrival_time'),
+        ('submission_queue', '0002_stop_updating_arrival_time'),
     ]
 
     operations = [
