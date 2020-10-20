dcca978701016cbd62a384eb49a5fecb013909fb
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-10-20 18:59:35 +0500

    mysql5.7 upgrade

--- submission_queue/tests/test_passive_grader.py
+++ submission_queue/tests/test_passive_grader.py
@@ -45,33 +45,6 @@ class PassiveGraderTest(TransactionTestCase):
     # in Jenkins
     QUEUE_NAME = 'test_queue_%s' % uuid4().hex
 
-    def setUp(self):
-        """Set up the client and stubs to be used across tests."""
-        # Create the grader
-        self.grader = SimplePassiveGrader(PassiveGraderTest.GRADER_RESPONSE)
-
-        # Create the response listener
-        # and configure it to receive messages on a local port
-        self.response_listener = GradeResponseListener()
-
-        # Create the client (input submissions)
-        # and configure it to send messages
-        # that will be sent back to our response listener
-        self.client = XQueueTestClient(self.response_listener.port_num())
-
-        # Create the user and make sure we are logged in
-        XQueueTestClient.create_user('test', 'test@edx.org', 'password')
-        self.client.login(username='test', password='password')
-
-        # Start up workers to pull messages from the queue
-        # and forward them to our grader
-        self.grader.start_workers(PassiveGraderTest.QUEUE_NAME)
-
-    def tearDown(self):
-        """Stop each of the listening services to free up the ports"""
-        self.grader.stop()
-        self.response_listener.stop()
-
     def test_submission(self):
         """Submit a single response to the XQueue and check that
         we get the expected response."""
