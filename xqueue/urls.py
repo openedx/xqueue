094febc395b6e196c4e893307d29e18fa997c9ac
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-01-21 14:48:14 +0500

    chore: Post Django32 Cleanup

--- xqueue/urls.py
+++ xqueue/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 
 urlpatterns = [
-    url(r'^xqueue/', include('submission_queue.urls')),
+   re_path(r'^xqueue/', include('submission_queue.urls')),
 ]
