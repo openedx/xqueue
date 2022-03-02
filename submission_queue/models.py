3c6a61fc0ea0d2e60299bb3c2a2dd9302313559a
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-03-03 00:30:40 +0500

    build: Running tests with mysql80 and mysql57.

--- submission_queue/models.py
+++ submission_queue/models.py
@@ -154,7 +154,3 @@ class Submission(models.Model):
         Alias for `s3_urls` field.
         '''
         return self.s3_urls
-
-
-class UserLearnerAssessmentDataTranslationAuditDepartmentTest(models.Model):
-    user_id_test = models.PositiveIntegerField(blank=False, null=False, db_index=True)
