7f9f0cab52c1cc50ffded5e09d6292589d016d0c
Muhammad Soban Javed <58461728+iamsobanjaved@users.noreply.github.com>
2023-09-26 18:36:14 +0500

    fix: use pytz while upgrading to avoid unexpected errors

--- xqueue/settings.py
+++ xqueue/settings.py
@@ -78,6 +78,11 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# Django 4.0+ uses zoneinfo if this is not set. We can remove this and
+# migrate to zoneinfo after Django 4.2 upgrade. See more on following url
+# https://docs.djangoproject.com/en/4.2/releases/4.0/#zoneinfo-default-timezone-implementation
+USE_DEPRECATED_PYTZ = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
